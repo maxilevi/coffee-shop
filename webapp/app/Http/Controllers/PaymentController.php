773c44b609fcfa85cdb2eecff15f99c165738593
Zaphyk <zaphyk@live.com>
2019-03-21 19:02:51 -0300

    add test user tokens

--- webapp/app/Http/Controllers/PaymentController.php
+++ webapp/app/Http/Controllers/PaymentController.php
@@ -9,6 +9,10 @@ use Log;
 
 class PaymentController extends Controller
 {
+    const SANDBOX_CLIENT_ID = "8165552372634154";
+    const SANDBOX_CLIENT_SECRET = "EKzGVQzDTAEclfi6abTOBe3mc1DQ3J6A";
+    const SANDBOX_ACCESS_TOKEN = "APP_USR-8165552372634154-032118-219f451f278a1bc962f0b9809ca88b5e-418906657";
+
     const CLIENT_ID = "4757717842404032";
     const CLIENT_SECRET = "2Y4pmHzCwJPoS9tTpXjLYIFnUtFYb8i7";
     const ACCESS_TOKEN = "APP_USR-4757717842404032-031710-9406a351eb88dc4a26a777de58eeb715-203380971";
@@ -18,8 +22,8 @@ class PaymentController extends Controller
     {
         $products = CartController::getProducts();
         if (count($products) === 0) return redirect('/');
-        MercadoPago\SDK::setClientId(self::CLIENT_ID);
-        MercadoPago\SDK::setClientSecret(self::CLIENT_SECRET);
+        MercadoPago\SDK::setClientId(self::IS_SANDBOX ? self::SANDBOX_CLIENT_ID : self::CLIENT_ID);
+        MercadoPago\SDK::setClientSecret(self::IS_SANDBOX ? self::SANDBOX_CLIENT_SECRET : self::CLIENT_SECRET);
 
         $preference = new MercadoPago\Preference();
         $preference->items = $this->getItems($products);
@@ -109,7 +113,7 @@ class PaymentController extends Controller
     public function ipn(Request $request)
     {
         Log::info("IPN RECEIVED topic='{$request->input('topic')}'");
-        MercadoPago\SDK::setAccessToken(self::ACCESS_TOKEN);
+        MercadoPago\SDK::setAccessToken(self::IS_SANDBOX ? self::SANDBOX_ACCESS_TOKEN : self::ACCESS_TOKEN);
         $merchant_order = null;
 
         switch($request->input('topic'))
