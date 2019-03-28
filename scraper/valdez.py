f0df241e159610e0621831d1c4c948a8965864b2
Zaphyk <zaphyk@live.com>
2019-03-28 14:48:05 -0300

    take into account shipping price

--- scraper/valdez.py
+++ scraper/valdez.py
@@ -9,7 +9,7 @@ def scrape(save_path, soup, params):
     formatter = image_formatter.ImageFormatter()
     data['name'] = params['name']
     data['description'] = params['description']
-    data['price'] = parse_price(soup.find('span', class_='price-tag-fraction').text) * 4 # Price per KG
+    data['price'] = parse_price(soup.find('span', class_='price-tag-fraction').text) * 4 + 300 # Price per KG
     data['cuts'] = ['Molido']
     thumbnail_url = params['thumbnail']
     data['thumbnail'] = '/temp/' + formatter.format(thumbnail_url, save_path)
